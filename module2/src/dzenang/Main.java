4c82f938d8c9588dc261a981d8cbeddceecc67bc
dzenang <dzenan.granulo@gmail.com>
2024-05-07 13:45:45 +0200

    Add resource bundle example

--- module2/src/dzenang/Main.java
+++ module2/src/dzenang/Main.java
@@ -1,5 +1,7 @@
 package dzenang;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import package1.Name;
 
 public class Main {
@@ -8,5 +10,10 @@ public class Main {
   public static void main(String[] args) {
     Name name = new Name();
     System.out.println("Hello " + name.getName());
+
+    ResourceBundle bundle = ResourceBundle.getBundle("localization/messages", Locale.US);
+    System.out.println("Greeting in English: " + bundle.getString("greeting"));
+    ResourceBundle frenchBundle = ResourceBundle.getBundle("localization/messages_fr_FR", Locale.FRENCH);
+    System.out.println("Greeting in French: " + frenchBundle.getString("greeting"));
   }
 }
